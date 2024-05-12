ea50090aa1c1fbbb3ec93bf46c887ba2c4cd2f43
Nikit Mehta <150411414+Ni-Kit-Mht@users.noreply.github.com>
2024-05-13 01:46:45 +0545

    Add files via upload

---
+++ nikitmehtajs.js
@@ -0,0 +1,9 @@
+var dropdownContent = document.getElementById("dropdownContent");
+
+function showDropdown() {
+    dropdownContent.style.display = "block";
+}
+
+function hideDropdown() {
+    dropdownContent.style.display = "none";
+}
