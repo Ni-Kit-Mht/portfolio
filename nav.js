8a0cd2ee65efaed8920702ec981b059669a1ce73
Nikit Mehta <150411414+Ni-Kit-Mht@users.noreply.github.com>
2025-05-12 11:45:39 +0545

    Update nav.js

--- nav.js
+++ nav.js
@@ -4,14 +4,3 @@ const toggleBtn = document.querySelector('.menu-toggle');
         toggleBtn.addEventListener('click', () => {
             navLinks.classList.toggle('active');
         });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    const currentPath = window.location.pathname;
-    const links = document.querySelectorAll('.nav-links a');
-
-    links.forEach(link => {
-      if (link.getAttribute('href') === currentPath) {
-        link.classList.add('activeSite');
-      }
-    });
-  });
