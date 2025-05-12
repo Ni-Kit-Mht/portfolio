75d25c6dd9292be7123ffbfe2bbac86a8d7704c7
Nikit Mehta <150411414+Ni-Kit-Mht@users.noreply.github.com>
2025-05-12 12:33:59 +0545

    Update navActive.js

--- navActive.js
+++ navActive.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const currentPath = window.location.pathname;
+  console.log(currentPath)
   const links = document.querySelectorAll('.nav-links a');
-
+  console.log(links)
   links.forEach(link => {
+    console.log(link)
+    console.log(link.getAttribut('href'))
     if (link.getAttribute('href') === currentPath) {
       link.classList.add('activeSite');
     }
