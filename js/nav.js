935001afd3740c45eb16c9778b71d5b43ec13852
Nikit Mehta <150411414+Ni-Kit-Mht@users.noreply.github.com>
2025-05-12 11:38:29 +0545

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,3 @@
-  document.addEventListener('DOMContentLoaded', () => {
-    const currentPath = window.location.pathname;
-    const links = document.querySelectorAll('.nav-links a');
-
-    links.forEach(link => {
-      if (link.getAttribute('href') === currentPath) {
-        link.classList.add('activeSite');
-      }
-    });
-  });
-
 const toggleBtn = document.querySelector('.menu-toggle');
         const navLinks = document.querySelector('.nav-links');
         
